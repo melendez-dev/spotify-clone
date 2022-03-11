2da63fd7288657965c4bde3e8bb2b4babfbc7153
kali <sams20030818@gmail.com>
2022-03-11 13:40:07 -0500

    feat(Root): add env.js

---
+++ env.js
@@ -0,0 +1,2 @@
+// authorization spotify
+export const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=741c3f6f609d4219bf2974cc40b43326&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
