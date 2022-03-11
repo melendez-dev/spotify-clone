6a369b20b9e0d721948e596d2d7386b6d6cff134
kali <sams20030818@gmail.com>
2022-03-11 16:52:51 -0500

    feat(Root): add a comment en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+// important to add style of chakra-ui
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
