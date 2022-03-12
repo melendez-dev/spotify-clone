457047c3436a49a6857919ad3cea24a3f5bd88f0
kali <sams20030818@gmail.com>
2022-03-12 16:40:20 -0500

    feat(Root): improve the app.jsx inteface

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 // import chakra-ui
-import { Box, Center } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { Login } from './components/Login'
 // import components
 import Dashboard from './components/Dashboard'
 import ToggleDarkMode from './components/ToggleDarkMode'
-// code in params
+// rquire code in the params url
 const code = new URLSearchParams(window.location.search).get('code')
 
 function App () {
   return (
     <Box overflow='hidden'>
       <ToggleDarkMode />
-      <Center mt={5}>
+      <Box>
         {code ? <Dashboard code={code} /> : <Login />}
-      </Center>
+      </Box>
     </Box>
   )
 }
