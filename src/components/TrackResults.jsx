5c1661b910bf00f1cb8b0760badb52546f72920c
kali <sams20030818@gmail.com>
2022-03-12 16:42:12 -0500

    chore(Root): add the component trackResult

---
+++ src/components/TrackResults.jsx
@@ -0,0 +1,20 @@
+// import chakra
+import { Flex, Image, Heading, Text, Box } from '@chakra-ui/react'
+import React from 'react'
+
+const TrackResults = ({ track, chooseTrack }) => {
+  function handlePlay () {
+    chooseTrack(track)
+  }
+  return (
+    <Flex m={2} alignItems='center' cursor='pointer' onClick={handlePlay}>
+      <Image src={track.albumUrl} alt={track.artists} borderRadius='full' />
+      <Box ml={2}>
+        <Heading size='md'> {track.title} </Heading>
+        <Text color='gray.500' isTruncated>{track.artist}</Text>
+      </Box>
+    </Flex>
+  )
+}
+
+export default TrackResults
